a137c9c9ef80da622b68491b6e46eb800ec3858e
lberthaut <louis.berthaut@gmx.com>
2021-04-09 16:25:19 +0200

    debut montage page photographes

--- js-json/scriptindex.js
+++ js-json/scriptindex.js
@@ -9,7 +9,7 @@ fetch('FishEyeDataFR.json')
 			photographersElement.innerHTML += `<article alt="photographers profile">
 						<a href="" alt="photographers page">
 							<div class="photo_graphers">
-								<img src="photos/sample/id_photos/${photographers.portrait}" alt="Mimi Keel" class="format_photos">
+								<img src="photos/sample/id_photos/${photographers.portrait}" alt="photographers_photo" class="format_photos">
 							</div>
 							<p class="photographers_name" alt="photographers name">
 								${photographers.name}
@@ -27,11 +27,18 @@ fetch('FishEyeDataFR.json')
 						<nav aria-label="categories" id="tagsbox">
 						</nav>
 					</article>`;
-		function includeTags (){
+					var photographersTagsBox = document.getElementById('tagsbox');
+					for(const photographersTags of photographers.tags){
+						let i=0; i<photographersTags.length; i++;
+						photographersTagsBox.innerHTML += `<input type="button" value="#${photographers.tags[i]}" alt="${photographers.tags[i]}" class="btn_select"></input>`
+					}
+		/* function includeTags (){
 			var photographersTagsBox = document.getElementById('tagsbox');
 			for(let i=0; i<photographers.tags.length; i++)
 			photographersTagsBox.innerHTML += `<input type="button" value="#${photographers.tags[i]}" alt="${photographers.tags[i]}" class="btn_select"></input>`
 		}
-		photographers.tags.forEach(includeTags);
+		photographers.tags.forEach(includeTags); */
 		}
 	})
+
+
